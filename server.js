60e01034232a2022e01c24b5b2a383949e4d69fb
AhmedHossam777 <ahmedhossam9226@gmail.com>
2025-08-09 21:42:13 +0300

    first session in CRUD operations

--- server.js
+++ server.js
@@ -1,55 +1,88 @@
-const express = require( 'express' );
+const express = require("express");
 
 const app = express();
 
-const users = [{
-	id: 1,
-	name: 'mohamed',
-	age: 22,
-}, {
-	id: 2,
-	name: 'ibrahim',
-	age: 40,
-}];
+const Users = [
+  {
+    id: 1,
+    name: "ahmed",
+    age: 20,
+  },
+  {
+    id: 2,
+    name: "mohamed",
+    age: 30,
+  },
+  {
+    id: 3,
+    name: "ali",
+    age: 22,
+  },
+  {
+    id: 4,
+    name: "ibrahim",
+    age: 30,
+  },
+];
 
-app.get( '/:id', ( req, res ) => {
-	const id = parseInt( req.params.id );
-	console.log( typeof id );
-	const user = users.find( u => u.id === id );
-	res.json( user );
-} );
+// get all users
+app.get("/users", (req, res) => {
+  res.status(200).json({
+    message: "get all users",
+    Users,
+  });
+});
 
-app.get('/',(req, res) => {
-	// // get all mobiles
-	// console.log(req.query.type);
-	// // select * from products where type is equal mobile
-	console.log(req.headers);
-	res.send('all users')
-	
-})
+app.get("/users/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const user = Users.find((user) => user.id === id);
 
-app.get( '/posts', ( req, res ) => {
-	res.send( 'this is all posts from backend' );
-} );
+  if (!user) {
+    res.status(404).json({
+      message: "user not found",
+    });
+  }
 
-// app.post('/login', (req, res)=>{
-// 	const usersData = req.
+  res.status(200).json({
+    message: "user fetched successfully",
+    user,
+  });
+});
+
+// get one user
+
+// // read:
+// app.get();
 //
-// 	// create user from data userData
-// })
+// // create:
+// app.post();
+//
+// // update:
+// app.patch();
+//
+// // delete:
+// app.delete();
 
 const port = 3000;
-app.listen( port, () => {
-	console.log( 'server is running on port 3000' );
-} );
 
-//? Request
-//?
-//?
-//? params -> :
-//? query -> ?
-//? body ->
-//? headers ->
-//?
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
 
-// jwt -> key => expiration time
+//! Status Code:
+// GET, POST, PATCH, PUT,DELETE
+// status code: HTTP protocol
+// 200 -> success(ok), 201 -> created, 204 => no content (delete)
+// 300 -> redirect
+// 400 -> bad request, 404 -> not found
+// 500 -> server error
+
+//! Request Object content:
+// req.params
+// req.query
+// req.headers
+
+// token -> key -> 30 day
+
+//! CRUD operations
+// create, read(get), update, delete
