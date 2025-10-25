adda254a4b22f0e2b73a3f11cc74709c9d4ed613
AhmedHossam777 <ahmedhossam9226@gmail.com>
2025-10-25 20:08:50 +0300

    .

--- model/User.js
+++ model/User.js
@@ -1,27 +1,27 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    age: {
-      type: Number,
-    },
-  },
-  {
-    timestamps: true,
-  },
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		age: {
+			type: Number,
+		},
+	},
+	{
+		timestamps: true,
+	}
 );
 
 // schema.pre('save', )
@@ -29,14 +29,16 @@ const userSchema = new mongoose.Schema(
 // 123456
 // user -> password => dsakldsakdjsalkdjaskldjaskl
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre('save', async function (next) {
+	if (!this.isModified('password')) {
+		return next();
+	}
 
-  this.password = await bcrypt.hash(this.password, 10);
+	this.password = await bcrypt.hash(this.password, 10);
 
-  next();
+	next();
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = { User };
