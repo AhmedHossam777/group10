b18397b7739b38fca96150bdb2a2daff67ebd86c
AhmedHossam777 <ahmedhossam9226@gmail.com>
2025-12-06 22:50:07 +0200

    virtuals

--- model/Post.js
+++ model/Post.js
@@ -1,31 +1,42 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    content: {
-      type: String,
-      required: true,
-    },
-    likes: {
-      type: Number,
-    },
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-  },
-  {
-    versionKey: false,
-    timestamps: true,
-  },
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		content: {
+			type: String,
+			required: true,
+		},
+		likes: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'User',
+			},
+		],
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+		},
+	},
+	{
+		versionKey: false,
+		timestamps: true,
+		virtuals: true,
+	}
 );
 
-const Post = mongoose.model("Post", postSchema);
+const Post = mongoose.model('Post', postSchema);
+
+postSchema.virtual('likesCount').get(function () {
+	return this.likes.length;
+});
+
+postSchema.set('toJSON', { virtuals: true });
+postSchema.set('toObject', { virtuals: true });
 
 module.exports = {
-  Post,
+	Post,
 };
--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -1,17 +1,20 @@
-const express = require("express");
-const { auth } = require("../middlewares/auth");
+const express = require('express');
+const { auth } = require('../middlewares/auth');
 
 const postRouter = express.Router();
 const {
-  updatePost,
-  deletePost,
-  getOnePost,
-  getAllPosts,
-  createPost,
-} = require("../controllers/postsController");
+	updatePost,
+	deletePost,
+	getOnePost,
+	getAllPosts,
+	createPost,
+	likePost,
+} = require('../controllers/postsController');
 
-postRouter.route("/").get(getAllPosts).post(auth, createPost);
+postRouter.route('/').get(getAllPosts).post(auth, createPost);
 
-postRouter.route("/:id").get(getOnePost).patch(updatePost).delete(deletePost);
+postRouter.route('/:id').get(getOnePost).patch(updatePost).delete(deletePost);
+
+postRouter.route('/:id/like').post(auth, likePost);
 
 module.exports = postRouter;
